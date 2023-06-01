adfa7de665df6bb1f74daa7e09e106ac4c4dc050
guiijanuario <guilherme.januario@zup.com.br>
2023-06-01 09:12:33 -0300

    Exercicio 6 finalizado

--- src/Exercicio6.java
+++ src/Exercicio6.java
@@ -1,25 +1,29 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Exercicio6 {
-  /*  public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Integer> numeros = new ArrayList<>();
 
         System.out.println("Insira o primeiro número inteiro: ");
         int num1 = scanner.nextInt();
         System.out.println("Insira o segundo número inteiro: ");
         int num2 = scanner.nextInt();
 
-        int contador = 2;
-
-        if (num1 > num2){
-            int contador = num1;
-        } else {
-            int contador = num2;
+        if ( num1 > num2){
+            for( int i = num2 + 1; i < num1; i++){
+                numeros.add(i);
+            }
+        }else if (num2 > num1){
+            for( int i = num2 - 1; i >= num1; i--){
+                numeros.add(i);
+            }
         }
 
-        for(int i = 1; i <= contador; i++ ){
-            System.out.println(i);
-        }
+        System.out.println("O intervalo entre os números que você inseriu é: " + numeros);
 
-    }*/
+
+    }
 }
