313a828f7353cbf489fb4817dc8bdfa99de07d2d
guiijanuario <guilherme.januario@zup.com.br>
2023-05-31 21:31:25 -0300

    Subindo Exercicio 5 pronto com ArrayList

--- src/Exercicio5.java
+++ src/Exercicio5.java
@@ -1,10 +1,20 @@
+import java.util.ArrayList;
+
 public class Exercicio5 {
     public static void main(String[] args) {
+
         System.out.println("[------- Ver quais são os números impares até 50 --------]");
+        ArrayList<Integer> impar = new ArrayList<>();
+        ArrayList<Integer> par = new ArrayList<>();
+
         for (int i = 1; i <= 50; i++){
             if (i % 2 == 0){
-                System.out.println(i);
+                par.add(i);
+            } else {
+                impar.add(i);
             }
         }
+
+        System.out.println("Esses números são impares: " + impar);
     }
 }
